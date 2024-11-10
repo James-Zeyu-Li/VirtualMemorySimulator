6a54014647fe49bc3fcfe7be5090f0b78bfd3092
James_ZY_L <zeyujames@gmail.com>
2024-11-10 11:05:33 -0800

    Update physicalFramManager to header and cpp file

--- PageTable/physicalFrameManager.cpp
+++ PageTable/physicalFrameManager.cpp
@@ -3,51 +3,42 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+#include "physicalFrameManger.h"
 
-class PhysicalFrameManager
+PhysicalFrameManager::PhysicalFrameManager(uint32_t totalFrames) : totalFrames(totalFrames)
 {
-private:
-    queue<uint32_t> freeFrames; // to store the free frames
-    uint32_t totalFrames;
-
-public:
-    // Constructor to initialize the total number of frames
-    // used in page table to initialize
-    PhysicalFrameManager(uint32_t totalFrames) : totalFrames(totalFrames)
+    for (uint32_t i = 0; i < totalFrames; ++i)
     {
-        for (uint32_t i = 0; i < totalFrames; ++i)
-        {
-            freeFrames.push(i);
-        }
+        freeFrames.push(i);
     }
+}
 
-    // allocate a frame, if no free frames return -1,
-    // used in pageTable page replacement
-    uint32_t allocateFrame()
+// allocate a frame, if no free frames return -1,
+// used in pageTable page replacement
+uint32_t PhysicalFrameManager::allocateFrame()
+{
+    if (freeFrames.empty())
     {
-        if (freeFrames.empty())
-        {
-            return -1;
-        }
-        uint32_t frame = freeFrames.front();
-        freeFrames.pop();
-        return frame;
+        return static_cast<uint32_t>(-1); // Return -1 if no frames are available
     }
+    uint32_t frame = freeFrames.front();
+    freeFrames.pop();
+    return frame;
+}
 
-    // free a frame, if the frame is invalid, print error message
-    void freeAFrame(uint32_t frame)
+// free a frame, if the frame is invalid, print error message
+void PhysicalFrameManager::freeAFrame(uint32_t frame)
+{
+    if (frame >= totalFrames)
     {
-        if (frame < 0 || frame >= totalFrames)
-        {
-            throw invalid_argument("Invalid frame number: " + to_string(frame));
-        }
-        freeFrames.push(frame);
+        throw std::invalid_argument("Invalid frame number: " + std::to_string(frame));
     }
+    freeFrames.push(frame);
+}
 
-    // get the total number of frames
-    // used in page fault to check if page replacement is needed
-    int getTotalFrames() const
-    {
-        return totalFrames;
-    }
-};
+// get the total number of frames
+// used in page fault to check if page replacement is needed
+uint32_t PhysicalFrameManager::getTotalFrames() const
+{
+    return totalFrames;
+}
